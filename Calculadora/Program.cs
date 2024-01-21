42c5cbc143dc5dfb17b310e281b1b17925f813dd
Natália Simões <natalia.simoes.dev@gmail.com>
2024-01-21 18:55:38 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora/Program.cs
@@ -0,0 +1,67 @@
+﻿Console.WriteLine("Qual operação deseja fazer");
+Console.WriteLine("1 - Adição");
+Console.WriteLine("2 - Subtração");
+Console.WriteLine("3 - Multiplicação");
+Console.WriteLine("4 - Divisão \n");
+
+int operacao = int.Parse(Console.ReadLine()); //converte para int
+
+Console.WriteLine("Digite o primeiro número");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo número");
+int num2 = int.Parse(Console.ReadLine());
+
+int resultado = 0;
+
+switch (operacao)
+{
+    case 1:
+        {
+            resultado = Adicao(num1, num2); break;
+        }
+    case 2:
+        {
+            resultado = Subtracao(num1, num2); break;
+        }
+    case 3:
+        {
+            resultado = Multiplicacao(num1, num2); break;
+        }
+    case 4:
+        {
+            resultado = Divisao(num1, num2); break;
+        }
+    default:
+        {
+            Console.WriteLine("Número inválido, digite outro número");
+            break;
+        }
+}
+
+Console.WriteLine("O resultado da operação é: {0}", resultado);
+
+
+static int Adicao(int numero1, int numero2)
+{
+    int result = numero1 + numero2;
+    return result;
+}
+
+static int Subtracao(int numero1, int numero2)
+{
+    int result = numero1 - numero2;
+    return result;
+}
+
+static int Multiplicacao(int numero1, int numero2)
+{
+    int result = numero1 * numero2;
+    return result;
+}
+
+static int Divisao(int numero1, int numero2)
+{
+    int result = numero1 / numero2;
+    return result;
+}
